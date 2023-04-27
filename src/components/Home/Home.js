9facbc9dc502c18b09925cc561d8f3b717eb5202
Otmane Aatik <aaotmane@gmail.com>
2023-04-27 15:31:52 -0400

    main component/nav created and styled

---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./styles.module.css";
+import { logOut } from "../utilities/users-service";
+
+const Home = ({user, setUser}) => {
+    const handleLogOut = () => {
+      logOut();
+      setUser(null);
+      window.location.reload();
+    };
+  return (
+    <div className={styles.main_container}>
+      <nav className={styles.navbar}>
+        <h1>Connectify</h1>
+        <button className={styles.white_btn} onClick={handleLogOut}>
+          Logout
+        </button>
+      </nav>
+    </div>
+  );
+}
+
+export default Home
