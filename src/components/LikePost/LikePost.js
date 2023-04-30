9cea52a90d4b948b33c1013d594b818039c52d75
Otmane Aatik <aaotmane@gmail.com>
2023-04-30 14:58:46 -0400

    like/dislike Posts

--- src/components/LikePost/LikePost.js
+++ src/components/LikePost/LikePost.js
@@ -1,32 +1,45 @@
-import axios from 'axios';
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const LikePost = ({post, user}) => {
+const LikePost = ({ post, user }) => {
+  const [userLiked, setUserLiked] = useState(false);
 
-    const [userLiked, setUserLiked] = useState(false);
-
-    useEffect(() => {
-      if (post.likes) {
-        if (post.likes.includes(user)) {
-          setUserLiked(true);
-        } else {
-          setUserLiked(false);
-        }
+  useEffect(() => {
+    if (post.likes) {
+      if (post.likes.includes(user)) {
+        setUserLiked(true);
+      } else {
+        setUserLiked(false);
       }
-    }, [user]);
+    }
+  }, [user]);
 
-    const likePost = () => {
-      axios.patch("http://localhost:3001/post/like-post/" + post._id, { user });
-    //   console.log(user)
-      setUserLiked(true);
-    };
+  const likePost = () => {
+    axios.patch("http://localhost:3001/post/like-post/" + post._id, { user });
+    setUserLiked(true);
+  };
+
+  const dislikePost = () => {
+    axios.patch("http://localhost:3001/post/dislike-post/" + post._id, {
+      user,
+    });
+    setUserLiked(false);
+  };
 
   return (
     <div className="like-icon">
-        <p>{post.likes ? post.likes.length : 0}</p>
-        <span id="dislike-btn" onClick={() => likePost()} >&#9829;</span>
+      <p>{post.likes ? post.likes.length : 0}</p>
+      {userLiked ? (
+        <span id="like-btn" onClick={() => dislikePost()}>
+          &#9829;
+        </span>
+      ) : (
+        <span id="dislike-btn" onClick={() => likePost()}>
+          &#9829;
+        </span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LikePost
+export default LikePost;
