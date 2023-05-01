643873dafdcdfd38ade4a4c14b387741a717cb8d
Otmane Aatik <aaotmane@gmail.com>
2023-05-01 11:00:37 -0400

    Delete component / CRUD completed

--- src/components/DeletePost/DeletePost.js
+++ src/components/DeletePost/DeletePost.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import './DeletePost.css';
 
-const DeletePost = () => {
+const DeletePost = ({ postId }) => {
+    // console.log(postId)
+
+  const handleDelete = () => {
+    axios.delete("http://localhost:3100/post/" + postId);
+  };
+
   return (
-    <div>DeletePost</div>
-  )
-}
+    <span id="delete-btn" onClick={() => handleDelete()}>
+      &#10010;
+    </span>
+  );
+};
 
-export default DeletePost
+export default DeletePost;
